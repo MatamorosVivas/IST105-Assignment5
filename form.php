bd04305ff4ad16f5f49c1c4c1c824c96a9f6f053
MatamorosVivas <c.matamoros.v2@gmail.com>
2025-02-11 21:44:51 -0800

    Initial commit - Added form.php, process.py, and process.php

--- form.php
+++ form.php
@@ -6,10 +6,10 @@
 <body>
     <h2>Welcome to the Interactive Treasure Hunt!</h2>
     <form action="process.php" method="post">
-        <label for="number">Enter a Number (e.g., birth year):</label>
+        <label for="number">Enter a Number :</label>
         <input type="number" name="number" required><br><br>
         
-        <label for="text">Enter a Text Message (e.g., name or secret word):</label>
+        <label for="text">Enter a Text Message :</label>
         <input type="text" name="text" required><br><br>
         
         <input type="submit" value="Solve the Puzzle">
