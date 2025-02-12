be0af39b7c35b61d6577b4602cfa74b1f7832c88
MatamorosVivas <c.matamoros.v2@gmail.com>
2025-02-12 03:38:25 -0800

    Initial commit - Added form.php, process.py, and process.php

--- process.php
+++ process.php
@@ -1,21 +1,18 @@
-<?php
-// Get inputs from form.php by using their names attributes
-$number = $_POST['number'];
-$text = $_POST['text'];
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Treasure Hunt Results</title>
+</head>
+<body>
+    <h2>Treasure Hunt Results</h2>
+    <?php
+    $number = $_POST["number"];
+    $text = $_POST["text"];
 
-// just checking if there are not empty, even though they have the "required" attribute in the input fields
-if (empty($number) || empty($text)) {
-    die("Please, complete all the fields");
-}
-
-// executing python script sending the arguments
-//escapeshellcmd prevents to execute the text received as a command
-$command = escapeshellcmd("python process.py $number $text");
-//executing and recieving the response from the python script
-$output = shell_exec($command);
-
-// printing the output values in the html, and adding some css style
-echo"<style>body { background-color:black; color:white;} .asw_container {display:flex; flex-direction:column; margin-bottom:10px;} .answer::before {content:'-'}</style>";
-echo "$output";
-
-?>
+    $command = escapeshellcmd("python process.py $number $text");
+    $output = shell_exec($command);
+    
+    echo $output;
+    ?>
+</body>
+</html>
